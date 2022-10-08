f027ed7e96547ee9b903e0f435a62375564a3d5d
frizzoana <frizzoanapaula04@gmail.com>
2022-10-07 21:43:02 -0300

    aula 02 js

---
+++ JS/02_ConsoleVariáveisEOperadores/main.js
@@ -0,0 +1,60 @@
+
+/*concatenar texto*/
+
+var A = "Colin";
+
+var B = "Faltou na";
+
+var C = "sexta-feira";
+
+console.log(A, B, C)  
+
+
+
+
+/*sinal + e , dão o mesmo resultado*/
+
+/*Colin faltou na sexta-feira*/
+
+
+
+/*   \n quebra a linha, como a tag br
+     
+     para colocar "" ou '' é preciso colocar \"  ou \'  */
+
+
+
+
+var Heloisa = 10;
+var Marcelo = 15;
+
+console.log(Heloisa + Marcelo)
+console.log(Heloisa - Marcelo)
+
+
+
+
+var Heloisa = 10;
+var Marcelo = 15;
+
+console.log(Heloisa < Marcelo) //true
+console.log(Heloisa > Marcelo) //false
+console.log(Heloisa = Marcelo) // heloisa passa a receber o valor de marcelo
+console.log(Heloisa == Marcelo) //false    dois sinais seria uma pergunta para saber se eles são iguais 
+
+
+
+var Heloisa = 10;
+var Marcelo = '15';
+
+console.log(Heloisa === Marcelo)  //pergunta se os tipos de variaveis e o valor são  iguais 
+
+
+
+
+console.log(Heloisa != Marcelo) // ! faz com que o resultado (false ou true) seja invertido
+
+
+/* a var pode conter string ou number
+   a let não pode declarar duas variáveis com o mesmo nome
+   a const não pode ser redeclarada e nem mudar o valor*/
