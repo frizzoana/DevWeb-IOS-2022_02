2caf26f0f52bb4deb90de7e8b0810449859fb52a
frizzoana <frizzoanapaula04@gmail.com>
2022-10-25 20:54:14 -0300

    aula 11 JS

---
+++ JS/11_DOMParte1/exercicio/main.js
@@ -0,0 +1,10 @@
+//alerta
+window.alert('Bem-vindo!');
+
+//titulo
+document.title = "Exercício, Dom parte 1";
+
+//section
+let section = section.createElement('H1', 'p');
+document.body.appendChild(section);
+section.innerHTML = 'Olá alunos!', 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis tempora sapiente ipsum consectetur accusantium cumque ipsam nisi ducimus quibusdam illo molestiae maiores labore asperiores, autem distinctio obcaecati aliquid earum praesentium!';
