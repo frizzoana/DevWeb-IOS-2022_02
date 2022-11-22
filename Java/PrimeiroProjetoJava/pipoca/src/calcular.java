ac4d4ede7b8d5121f5570d06d1839fda8ba3c952
frizzoana <frizzoanapaula04@gmail.com>
2022-11-22 18:47:09 -0300

    JAVA

---
+++ Java/PrimeiroProjetoJava/pipoca/src/calcular.java
@@ -0,0 +1,11 @@
+public class calcular {
+    //primeiro passo criar metodo main
+    public static void main(String[] args){
+        //vamos somar a idade de tres alunos
+        //var idade1 = 25
+        int idade1 = 25;
+        int idade2 = 15;
+        int somaidade = idade1 + idade2;
+        System.out.println("A soma das idades dos alunos é: " + somaidade);  //println pula p linha de baixo
+    }
+}
---
+++ Java/PrimeiroProjetoJava/pipoca/src/pipoca.java
@@ -0,0 +1,9 @@
+public class pipoca {
+    //Mostra no console a frase Hello Word
+    public static void main(String[] args){
+        String texto = "Hello Word";
+        System.out.println(texto);
+        System.out.print(45);
+        //console.log("HelloWord")
+    }
+}
