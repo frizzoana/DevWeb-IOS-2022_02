7a6fd129223a50cb9390da6bcbe01dc15d111307
frizzoana <frizzoanapaula04@gmail.com>
2022-11-22 21:27:46 -0300

    exercicio java 03

---
+++ Java/Exercicio_Java_03/Exercicio_Java_03/src/CalcularAreaTrianguloRetangulo.java
@@ -0,0 +1,8 @@
+public class CalcularAreaTrianguloRetangulo {
+    public static void main(String[] args) {
+        int base = 8;
+        int altura = 10;
+        int area = (base * altura) / 2;
+        System.out.println("A área do triângulo é: " + area);
+    }
+}
---
+++ Java/Exercicio_Java_03/Exercicio_Java_03/src/CalcularMediaDeNotas.java
@@ -0,0 +1,10 @@
+public class CalcularMediaDeNotas {
+    public static void main(String[] args) {
+        int nota1 = 9;
+        int nota2 = 8;
+        int nota3 = 7;
+        int nota4 = 8;
+        int média = (nota1 + nota2 + nota3 + nota4) / 4;
+        System.out.println("A média do aluno é: " + média);
+    }
+}
